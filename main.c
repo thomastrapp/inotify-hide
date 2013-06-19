bf1ad9f28b139949129424f107ad45f5f8ef384d
Tom <tom@universe>
2013-06-19 04:52:09 +0200

    help message

--- main.c
+++ main.c
@@ -13,7 +13,8 @@ int main(int argc, char ** argv)
 {
   if( argc < 2 || strcmp(argv[1], "--help") == 0 || strcmp(argv[1], "-h") == 0 )
   {
-    print_error("Usage: %s file", argv[0]);
+    print_error("Usage: %s <file>\n"
+                "       Delete <file> and (maybe) restore", argv[0]);
     return EXIT_FAILURE;
   }
 
